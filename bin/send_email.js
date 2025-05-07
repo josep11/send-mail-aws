e1ca563d052bc6f4a1dbea864b01aa0ed0181cca
Josep Alsina <josepalsinasio@gmail.com>
2025-05-07 17:37:14 +0200

    .

--- bin/send_email.js
+++ bin/send_email.js
@@ -3,21 +3,21 @@ const argv = require("yargs").argv;
 // console.log(`clu running on ${process.cwd()}`);
 
 const printHelp = function () {
-  process.stdout.write("\nUSAGE: \n");
-  process.stdout.write(
-    "send-mail-aws -s <title> [-b body] [-r me@it.com] [-h]\n",
-  );
-  process.stdout.write(
-    "send-mail-aws --subject=<title> [--body=body] [--receiver=me@gmail.com] [-h]\n",
-  );
+	process.stdout.write("\nUSAGE: \n");
+	process.stdout.write(
+		"send-mail-aws -s <title> [-b body] [-r me@it.com] [-h]\n",
+	);
+	process.stdout.write(
+		"send-mail-aws --subject=<title> [--body=body] [--receiver=me@gmail.com] [-h]\n",
+	);
 };
 
 const subject = argv.subject || argv.s;
 
 if (argv.h || !subject) {
-  //help or malformed command
-  printHelp();
-  process.exit(0);
+	//help or malformed command
+	printHelp();
+	process.exit(0);
 }
 
 let { body, b } = argv;
@@ -28,5 +28,5 @@ receiver = receiver || r || null;
 
 const { sendEmail } = require("../lib/AWS_SES_Wrapper");
 (async () => {
-  await sendEmail(subject, body, receiver);
+	await sendEmail(subject, body, receiver);
 })();
